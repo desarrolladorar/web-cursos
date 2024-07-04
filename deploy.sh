ef63b2dff386404bf38b1a5fe8e9c518a73a6170
Andre Rivera <andre_rivera2011@hotmail.com>
2024-07-03 23:54:53 -0500

    mas cambios 3

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,15 @@ REMOTE_DIR='/public_html'
 # Directorio local de tu proyecto
 LOCAL_DIR=$(pwd)
 
+# Cambia permisos localmente
+find $LOCAL_DIR -type d -exec chmod 755 {} \;
+find $LOCAL_DIR -type f -exec chmod 644 {} \;
+
 # Sincroniza los archivos usando lftp
 lftp -f "
 set ssl:verify-certificate no
 open $HOST
 user $USER $PASS
 mirror --reverse --delete --verbose --exclude .git --exclude deploy.sh $LOCAL_DIR $REMOTE_DIR
-chmod -R 755 $REMOTE_DIR
-find $REMOTE_DIR -type f -exec chmod 644 {} \;
 bye
 "
